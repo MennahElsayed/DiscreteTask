9d1fa7025180522799732e9bdd7de6d502318300
Reham-Elbaz <50484754+Reham-Elbaz@users.noreply.github.com>
2022-12-24 15:37:55 +0200

    perfect numbers

---
+++ Perfect_numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Perfect_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum;
+            Console.WriteLine("Please,Enter the start of the range :");
+            int start = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please,Enter the end of the range : ");
+            int end = int.Parse(Console.ReadLine());
+
+            for (int i = start; i <= end; i++)
+            {
+                sum = 0;
+                for (int factor = 1; factor < i; factor++)
+                {
+                    if (i % factor == 0)
+                    {
+                        sum += factor;
+                    }
+                }
+
+                if (sum == i)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
